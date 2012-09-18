3aae6a5dac1ea6065b31b7f6899f478754b52589
Ryan Burton <ryan@thoughtleadr.com>
2012-09-18 11:38:08 -0700

    Add `parMap` which caps parallelism to capabilities

--- Control/Concurrent/Multiprocessing.hs
+++ Control/Concurrent/Multiprocessing.hs
@@ -1,15 +1,22 @@
 module Control.Concurrent.Multiprocessing
-    (parallel, parallel_, parForM, parForM_, splitIntoBitsOf)
+    (parMap, parallel, parallel_, parForM, parForM_, splitIntoBitsOf)
     where
 
 import Prelude hiding (catch)
 import Data.Either (partitionEithers)
+import Control.DeepSeq
 import Control.Exception
 import Control.Monad
 import Control.Concurrent.MVar
 import GHC.Conc
 import Control.Concurrent.ParallelIO.Local hiding (parallel_)
+import qualified Control.Monad.Par as Par
 
+-- |
+--
+-- >>> splitIntoBitsOf 8 [1..20]
+-- [[1,2,3,4,5,6,7,8],[9,10,11,12,13,14,15,16],[17,18,19,20]]
+splitIntoBitsOf :: Int -> [a] -> [[a]]
 splitIntoBitsOf _ [] = [[]]
 splitIntoBitsOf n xs    | not $ null ys' = ys : (splitIntoBitsOf n ys')
                         | otherwise = [ys]
@@ -17,7 +24,8 @@ splitIntoBitsOf n xs    | not $ null ys' = ys : (splitIntoBitsOf n ys')
         ys = take n xs
         ys' = drop n xs
 
--- edited from Control/Concurrent/ParallelIO/Local.hs because original has space leak (defined as `parallel pool xs >> return ()`)
+-- edited from Control/Concurrent/ParallelIO/Local.hs because original has space leak (defined as @parallel pool xs >> return ()@)
+-- | 'parallel' with its result discarded.
 parallel_ :: Pool -> [IO a] -> IO ()
 parallel_ pool acts = mask $ \restore -> do
         main_tid <- myThreadId
@@ -34,6 +42,7 @@ parallel_ pool acts = mask $ \restore -> do
         reflectExceptionsTo :: ThreadId -> IO () -> IO ()
         reflectExceptionsTo tid act = act `catch` \e -> throwTo tid (e :: SomeException)
 
+-- | 'parallel' with its arguments flipped.
 parForM xs action = withPool numCapabilities $ \pool -> do
     let splitted = splitIntoBitsOf numCapabilities xs
     (errs, results) <- fmap (partitionEithers . concat) $
@@ -42,6 +51,7 @@ parForM xs action = withPool numCapabilities $ \pool -> do
             rs `pseq` return rs)
     return $ (errs, results)
 
+-- | 'parallel_' with its arguments flipped.
 parForM_ xs action = withPool numCapabilities $ \pool -> do
     let splitted = splitIntoBitsOf numCapabilities xs
     (errs, _) <- fmap (partitionEithers . concat) $
@@ -49,3 +59,10 @@ parForM_ xs action = withPool numCapabilities $ \pool -> do
             rs <- parallelInterleavedE pool s
             rs `pseq` return rs)
     print errs
+
+-- | Apply a given function to each argument in the list in parallel with
+-- execution batched to the number of capabilities (cores) of the machine
+-- at a time.
+parMap :: (NFData b) => (a -> b) -> [a] -> [b]
+parMap f xs = concat $ map (Par.runPar . Par.parMap f) $
+    splitIntoBitsOf numCapabilities xs
